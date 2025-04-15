145d28bb5257e86e5bd6278cca2d29979455614b
junyonglee0223 <ljyong3339@gmail.com>
2025-04-15 16:30:16 +0900

    feat: 구체 클래스 기반 Proxy 패턴 - 기존 Order 앱에 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,22 +1,18 @@
 package hello.proxy;
 
-import hello.proxy.app.v1.OrderControllerV1;
-import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_config.ConcreteProxyConfig;
 import hello.proxy.config.v1_config.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-import java.util.Arrays;
-
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import({InterfaceProxyConfig.class, AppV2Config.class})
+//@Import({InterfaceProxyConfig.class, AppV2Config.class})
+@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 //@SpringBootApplication
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
