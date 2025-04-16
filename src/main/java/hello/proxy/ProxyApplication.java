91682ab6ff15efc5610785975c470163549e17fa
junyonglee0223 <ljyong3339@gmail.com>
2025-04-16 16:02:25 +0900

    feat: add filter but not working

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -3,6 +3,7 @@ package hello.proxy;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +14,9 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import({InterfaceProxyConfig.class, AppV2Config.class})
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
-@Import({DynamicProxyBasicConfig.class, ConcreteProxyConfig.class})
+//@Import({DynamicProxyBasicConfig.class, ConcreteProxyConfig.class})
+@Import({DynamicProxyFilterConfig.class, ConcreteProxyConfig.class})
+
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
